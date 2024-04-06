7c877204e897867365c81e421ee2d6d3a1ead67b
Uchi <a26689@svalero.com>
2024-04-06 02:47:11 +0200

    nodejs

--- src/app.module.ts
+++ src/app.module.ts
@@ -17,7 +17,7 @@ const client = new Client({
   user: 'postgres',
   host: 'pgdb-msr.crum4wauqhme.us-east-1.rds.amazonaws.com',
   database: 'postgres',
-  password: '12345678',
+  password: '123456788',
   port: 5432,
   ssl: true,
 });
